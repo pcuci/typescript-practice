8b1a71ef1eb6a19b96d2c9d9aba8aa276c4dae7f
Paul Cuciureanu <paulcu@gmail.com>
2016-05-28 00:58:38 -0400

    Simple typescript type tests

--- hello_world.ts
+++ hello_world.ts
@@ -3,5 +3,25 @@ class HelloWorld {
   }
 }
 
-var hello = new HelloWorld("Hello TypeScript 1!!!");
+var hello = new HelloWorld("Hello TypeScript!");
 console.log(hello.message, 'again');
+
+function scopeTest() {
+  if (true) {
+    var foo = 'use anywhere';
+    let bar = 'use in this block';
+    // do some more stuff
+  }
+  console.log(foo); // works!
+  console.log(bar); // doesn't work!
+
+  let myString = 'this is a string';
+  myString = 42;
+
+  function returnNumber(): number {
+    return 42; // infers return type
+  }
+  let anotherString: string = 'this is also a string';
+  anotherString = returnNumber(); // error!
+}
+
